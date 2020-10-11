9e61f7c6bb34ea47e6eb2f8bb00029855f1505d8
Manu M <manu.m@tataelxsi.co.in>
2020-10-11 18:07:57 +0530

    tests for actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import './App.scss';
+import React, { Component } from 'react';
 import Header from './components/Header/Header'
 import Headline from './components/Headline/Headline';
+import SharedButton from './components/Button/Button';
+import ListItem from './components/ListItem/ListItem';
+import { connect } from 'react-redux';
+import { fetchPosts } from './actions';
+
+import './App.scss';
 
 const tempArr = [{
   fName: 'Joe',
@@ -11,15 +16,74 @@ const tempArr = [{
   onlineStatus: true
 }];
 
-function App() {
-  return (
-    <div className="App" data-test="appComponent">
-      <Header />
-      <section className="main">
-        <Headline header="Posts" desc="Click the button to render posts!" tempArr={tempArr} />
-      </section>
-    </div>
-  );
+const initialState = {
+  hideBtn: false
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState
+    }
+    this.fetch = this.fetch.bind(this);
+  }
+  fetch() {
+    this.props.fetchPosts();
+    this.exampleMethod_updatesState();
+  }
+
+  exampleMethod_updatesState() {
+    const { hideBtn } = this.state;
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  }
+
+  exampleMethod_returnsAValue(number) {
+    return number + 1;
+  }
+  render() {
+    const { posts } = this.props;
+    const { hideBtn } = this.state;
+
+    const configButton = {
+      buttonText: 'Get posts',
+      emitEvent: this.fetch
+    }
+    return (
+      <div className="App" data-test="appComponent">
+        <Header />
+        <section className="main">
+          <Headline header="Posts" desc="Click the button to render posts!" tempArr={tempArr} />
+          {!hideBtn &&
+            <SharedButton {...configButton} />
+          }
+          
+          {posts.length > 0 &&
+            <div>
+              {posts.map((post, index) => {
+                const { title, body } = post;
+                const configListItem = {
+                  title,
+                  desc: body
+                };
+                return (
+                  <ListItem key={index} {...configListItem} />
+                )
+              })}
+            </div>
+          } 
+        </section>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(App);
---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../../../Utils';
+import SharedButton from './Button';
+
+describe('SharedButton Component', () => {
+
+    describe('Checking PropTypes', () => {
+
+        it('Should NOT throw a warning', () => {
+            const expectedProps = {
+                buttonText: 'Example Button Text',
+                emitEvent: () => {
+
+                }
+            };
+            const propsError = checkProps(SharedButton, expectedProps);
+            expect(propsError).toBeUndefined();
+        });
+
+    });
+
+    describe('Renders', () => {
+
+        let wrapper;
+        let mockFunc;
+        beforeEach(() => {
+            mockFunc = jest.fn();
+            const props = {
+                buttonText: 'Example Button Text',
+                emitEvent: mockFunc
+            };
+            wrapper = shallow(<SharedButton {...props} />);
+        });
+
+        it('Should Render a button', () => {
+            const button = findByTestAttr(wrapper, 'buttonComponent');
+            expect(button.length).toBe(1);
+        });
+
+        it('Should emit callback on click event', () => {
+            const button = findByTestAttr(wrapper, 'buttonComponent');
+            button.simulate('click');
+            const callback = mockFunc.mock.calls.length;
+            expect(callback).toBe(1);
+        });
+
+
+    });
+
+
+
+
+});
